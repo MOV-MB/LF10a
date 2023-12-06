95f05e2a02cb5ccd21f6911c63fec4a753a2dc31
MOV-MB <31706230+MOV-MB@users.noreply.github.com>
2023-12-06 14:26:09 +0100

    Fixed Typo in package.json

--- Bindr/jest.config.js
+++ Bindr/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  preset: 'react-native',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation|@react-native-firebase/auth|@react-native-firebase/app)',
+  ],
 };
